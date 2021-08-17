620ec067cf0ceafc848b4141e94701f482497490
rosh <rosh.ramesh@banck-abc.com>
2021-08-17 07:04:01 +0530

    Added basic datatable

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+    auto:true,
+    autoStart:true,
+    infiniteLoop:true,
+    pause: 2000
+  });
+  $('.specials').bxSlider({
+    autoStart:true,
+    minSlides: 1,
+    maxSlides: 4,
+    slideWidth: 265,
+    slideMargin: 30,
+    responsive: true
+  });
+
+  $('#menu-table').DataTable();
+});
