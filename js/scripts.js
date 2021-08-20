595d97ab7dc02a6de2b6434c256a63e4ca4993a1
sijo <sijo.qln@gmail.com>
2021-08-20 01:48:25 -0400

    New changes added

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
 
-  
+
   var _items = [];
+  var lUserName="";
+  var lPassword="";
+  var validate = /^\s*[a-zA-Z0-9,\s]+\s*$/;
+  var acc = [lUserName, lPassword];
 
   let total= 0;
 
   class Item {
-    constructor( type, itemName, price) { 
+    constructor( type, itemName, price) {
       this.type = type;
       this.itemName = itemName;
       this.price = price;
@@ -43,7 +47,11 @@ $(document).ready(function(){
     responsive: true
   });
 
-  $("#menu-table").DataTable();
+  if ($('#menu-table').length)
+ {
+  $('#menu-table').DataTable();
+ }
+  // $("#menu-table").DataTable();
 
   $( "#menu-btn-1" ).on( "click", function( event ) {
     event.preventDefault();
@@ -92,7 +100,7 @@ $(document).ready(function(){
 
   if($("#order-table").length) {
     this._items.forEach(element => {
-      
+
     });
   }
 
@@ -108,8 +116,177 @@ $(document).ready(function(){
     $('.cart-data').remove();
     $("#total-amount").html(total);
   });
+  $('#create-button').on('click', function() {
 
-});
+    var cUsername = $("#create-username").val();
+    var cPassword = $("#create-password").val();
+    var cEmail = $("#create-email").val();
+    var cUsernameValid = false;
+    var cPasswordValid = false;
+    var cEmailValid = false;
+    var cUsernameObj = $("#create-username");
+    var cPasswordObj = $("#create-password");
+    var cEmailObj = $("#create-email");
+
+    var vEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if(!validate.test(cUsername) || (cUsername).length == 0) {
+      $(cUsernameObj).addClass("error");
+      $(cUsernameObj).val("Please enter a valid username.");
+    } else {
+      cUsernameValid = true;
+      var createUsername = $(cUsernameObj).val();
+    }
+
+    if(!validate.test(cPassword) || (cPassword).length == 0) {
+      $(cPasswordObj).addClass("error");
+      $(cPasswordObj).val("Password format is wrong.");
+    } else {
+      cPasswordValid = true;
+      var createPassword = $(cPasswordObj).val();
+    }
+
+    if(!vEmail.test(cEmail)) {
+      $(cEmailObj).addClass("error");
+      $(cEmailObj).val("Please enter a valid email");
+    } else {
+      cEmailValid = true;
+      console.log("Account Email " + cEmailObj.val());
+    }
+
+    $(cUsernameObj).on('click', function () {
+      $(this).val("");
+      $(this).removeClass("error");
+    });
+
+    $(cPasswordObj).on('click', function () {
+      $(this).val("");
+      $(this).removeClass("error");
+    });
+
+    $(cEmailObj).on('click', function () {
+      $(this).val("");
+      $(this).removeClass("error");
+    });
+
+    acc = [createUsername, createPassword];
+
+    if(cUsernameValid == true && cPasswordValid == true && cEmailValid == true) {
+      $('form').animate({
+      height: "toggle",
+      opacity: "toggle"
+    }, "fast");
+    }
+  });
+
+  $('#login-button').on('click', function() {
+
+    var lUserNameEntry = $("#login-username").val();
+    var lPasswordEntry = $("#login-password").val();
+    var cUsernameObj1 = $("#login-username");
+    var cPasswordObj1 = $("#login-password");
+    if(!validate.test(lUserNameEntry) || (lUserNameEntry).length == 0) {
+      $(cUsernameObj1).addClass("error");
+      $(cUsernameObj1).val("Please enter a valid username.");
+    }
+    if(!validate.test(lPasswordEntry) || (lPasswordEntry).length == 0) {
+      $(cUsernameObj1).addClass("error");
+      $(cPasswordObj1).val("Please enter a valid password.");
+    }
+    if (lUserNameEntry != acc[0] && lPasswordEntry != acc[1]) {
+      console.log(acc[0]+ " Error in Logging");
+    }
+  });
+
+  $('.message a').on('click', function() {
+    $("#create-username").val("");
+    $("#create-password").val("");
+    $("#create-email").val("");
+    $("#login-username").val("");
+    $("#login-password").val("");
+    $('form').animate({
+      height: "toggle",
+      opacity: "toggle"
+    }, "fast");
+  });
+
+  $('.btn-submit').on('click', function() {
+
+  var lname = $("#lname").val();
+  var lemail = $("#lemail").val();
+  var ltel = $("#ltel").val();
+  var lsubject = $("#lsubject").val();
+
+  console.log(lname)
+  // var cPasswordValid = false;
+  // var cEmailValid = false;
+  var lnameObj = $("#lname");
+  var lemailObj = $("#lemail");
+  var ltelObj = $("#ltel");
+  var lsubjectObj = $("#lsubject");
+  let isValid = true;
+  var vEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if(!validate.test(lname) || (lname).length == 0) {
+    $(lnameObj).addClass("error1");
+    $(lnameObj).val("Please enter a valid username.");
+    isValid = false;
+  } else {
+    clnameValid = true
+  }
+
+  if(!vEmail.test(lemail)) {
+    $(lemailObj).addClass("error1");
+    $(lemailObj).val("Please enter a valid email");
+    isValid = false;
+  } else {
+    clemailValid = true;
+  }
+
+  const phone_number = ltel.trim();
+          if (phone_number == "" || (isNaN(phone_number)) || phone_number.length != 10 ) {
+            $(ltelObj).addClass("error1");
+            $(ltelObj).val("Please enter a phone number");
+            isValid = false;
+          }
+
+
+  if(!validate.test(lsubject) || (lsubject).length == 0) {
+    $(lsubjectObj).addClass("error1");
+    $(lsubjectObj).val("Please enter a valid Subject")
+    isValid = false;
+  } else {
+    csubjectValid = true
+  }
+
+  $(lnameObj).on('click', function () {
+    $(this).val("");
+    $(this).removeClass("error1");
+  });
+
+  $(lemailObj).on('click', function () {
+    $(this).val("");
+    $(this).removeClass("error1");
+  });
+
+  $(ltelObj).on('click', function () {
+    $(this).val("");
+    $(this).removeClass("error1");
+  });
+
+  $(lsubjectObj).on('click', function () {
+    $(this).val("");
+    $(this).removeClass("error1");
+  });
+
+  if (isValid == false) {
+      evt.preventDefault();
+          }
+
+
+  });
+
+  });
 
 function openNav() {
   document.getElementById("login-modal").style.display = "block";
