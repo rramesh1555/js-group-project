56ba53715436573f2a049031072837c954a37c0f
rosh <rosh.ramesh@banck-abc.com>
2021-08-20 07:56:25 +0530

    datatable modified

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,32 @@
 $(document).ready(function(){
 
-  class items {
-    items() { 
-    }
+  
+  var _items = [];
 
+  let total= 0;
+
+  class Item {
+    constructor( type, itemName, price) { 
+      this.type = type;
+      this.itemName = itemName;
+      this.price = price;
+    }
     type = "";
     itemName = "";
     price = "";
 
+    get type()  {
+      return this.type;
+    }
 
+    get itemName()  {
+      return this.itemName;
+    }
+
+    get price()  {
+      return this.price;
+    }
   }
-
 
   $('.bxslider').bxSlider({
     auto:true,
@@ -29,4 +45,69 @@ $(document).ready(function(){
 
   $("#menu-table").DataTable();
 
+  // $("#menu-sel-btn").click(function(){
+  //   alert("helllooo");
+  // });
+
+  $( "#menu-btn-1" ).on( "click", function( event ) {
+    event.preventDefault();
+    let a = $( "#menu-btn-1" ).val();
+    var t = get(a);
+    _items.push(get(a));
+    updateCart(t, a);
+  });
+
+  $( "#menu-btn-2" ).on( "click", function( event ) {
+    event.preventDefault();
+    let a = $( "#menu-btn-2" ).val();
+    var t = get(a);
+    _items.push(get(a));
+    updateCart(t);
+  });
+
+  $( "#menu-btn-3" ).on( "click", function( event ) {
+    event.preventDefault();
+    let a = $( "#menu-btn-3" ).val();
+    var t = get(a);
+    _items.push(get(a));
+    updateCart(t);
+  });
+
+  var item1 = new Item("Breakfast", "Black Bean Dip", 16);
+  var item2 = new Item("Breakfast", "Bacon and Egg", 29);
+  var item3 = new Item("Breakfast", "Bagel and cream cheese", 43);
+
+  // item1.type = "test10";
+
+  var map = { one: item1, two: item2, three: item3 }
+  function get(k){
+    return map[k];
+  }
+
+  function updateCart(t, a) {
+    // var row = " <tr> <td>"+t.type+"</td> <td>"+t.itemName+"</td><td>"+t.price+"</td> <td><button class='remove_btn'></button></td></tr>";
+    // $("#menu-cart").append(row);
+    // $("#menu-cart").closest('table').find('tr:last').prev().after(" <tr> <td>"+t.type+"</td> <td>"+t.itemName+"</td><td>"+t.price+"</td> <td><button class='remove_btn'></button></td></tr>");
+
+    // $("#menu-cart").find('tr:last').prev().after(" <tr><td>"+t.type+"</td> <td>"+t.itemName+"</td><td>"+t.price+"</td> <td><button class='remove_btn'></button></td></tr>");
+    $("#menu-cart").find('tr:last').prev().after(" <tr><td>"+t.type+"</td> <td>"+t.itemName+"</td><td>"+t.price+"</td> <td><button class='remove_btn'></button></td></tr>");
+    total = total + parseFloat(t.price);
+    $("#total-amount").html(total);
+  }
+
+  if($("#order-table").length) {
+    this._items.forEach(element => {
+      
+    });
+  }
 });
+
+
+//modal 
+function openNav() {
+  document.getElementById("login-modal").style.display = "block";
+}
+
+function closeNav() {
+  document.getElementById("login-modal").style.display = "none";
+}
